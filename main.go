6062bcc57aa097ed19472becd617181992aa8137
David Godwin <dgodwin26@yahoo.co.uk>
2020-07-26 07:31:51 +0100

    Playing with interfaces and methods for a parser on a sunday morning In short, we want to allow a user to be able to parse several differnet kinds of files but have the function into that be relatively straightforward. One solution is to have an interface that implements one method Translate(). In this, we are able to have many differnet kinds of implementation, each with a core 'Translation' element that can, at a high level, take a file path and translate the given structure into something that the tool can understand.
    Then, in main, we can just call Parse() and expect a config back. The logic simply says 'I've got this file, I now need to call the appropriate translation implementation.

--- main.go
+++ main.go
@@ -1,7 +1,62 @@
 package main
 
+import (
+	"fmt"
+	"path/filepath"
+)
+
+// Represents a parsed configuration file.
+// Regardless of how a parser is implemented, they
+// all ought to be able to create a Config.
+type Config struct{
+	urls []string
+	count int
+
+}
+
+// A tiny interface with one method (ParseFile()) that is used to
+// to extract data from a file and create a Config object.
+// There can, and ought, to be multiple implementations of Parser
+// to suit different use cases — JSON, XML, CSV.
+type Parser interface{
+	Translate(file string) Config
+}
+
+type JSONParser struct{}
+
+func (j JSONParser) Translate(file string) Config{
+	fmt.Println("JSON parsing")
+	return Config{[]string{"JSON PARSED FILE"}, 2}
+}
+
+type XMLParser struct{}
+
+func (x XMLParser) Translate(file string) Config{
+	fmt.Println("XML parsing baby")
+	return Config{[]string{"XML parsing"}, 5}
+}
+
+
+// Consider creating a function that is called GetParser().
+// GetParser() takes in the file name and returns the appropriate
+// Parser object. When .ParseFile is run on the Parser object, then
+// the decoding happens.
+
+func Parse(path string) Config{
+	// Translate takes in a filepath and returns
+	// a parsed object for that file.
+	var Parser = map[string]Parser{
+		".xml":XMLParser{},
+		".json":JSONParser{},
+		//Add additional parsers here. They need to implement Parser interface.
+	}
+	return Parser[filepath.Ext(path)].Translate("path")
+
+}
+
+
+
 func main() {
-
-
-
+	Parse("hello.xml")
+	Parse("hello.json")
 }
