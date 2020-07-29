f85c51d05529c1604ea4214aa55398ba27cfdb21
David Godwin <dgodwin26@yahoo.co.uk>
2020-07-29 15:32:33 +0100

    Play with DoConcurrentTask()

--- main.go
+++ main.go
@@ -2,61 +2,68 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
+	"time"
 )
 
-// Represents a parsed configuration file.
-// Regardless of how a parser is implemented, they
-// all ought to be able to create a Config.
-type Config struct{
-	urls []string
-	count int
-
-}
-
-// A tiny interface with one method (ParseFile()) that is used to
-// to extract data from a file and create a Config object.
-// There can, and ought, to be multiple implementations of Parser
-// to suit different use cases — JSON, XML, CSV.
-type Parser interface{
-	Translate(file string) Config
-}
-
-type JSONParser struct{}
-
-func (j JSONParser) Translate(file string) Config{
-	fmt.Println("JSON parsing")
-	return Config{[]string{"JSON PARSED FILE"}, 2}
-}
-
-type XMLParser struct{}
-
-func (x XMLParser) Translate(file string) Config{
-	fmt.Println("XML parsing baby")
-	return Config{[]string{"XML parsing"}, 5}
-}
-
-
-// Consider creating a function that is called GetParser().
-// GetParser() takes in the file name and returns the appropriate
-// Parser object. When .ParseFile is run on the Parser object, then
-// the decoding happens.
-
-func Parse(path string) Config{
-	// Translate takes in a filepath and returns
-	// a parsed object for that file.
-	var Parser = map[string]Parser{
-		".xml":XMLParser{},
-		".json":JSONParser{},
-		//Add additional parsers here. They need to implement Parser interface.
+	func DoConcurrentTask(task func(), count int, ticker time.Ticker) {
+		// DoConcurrentTask takes in a function and run it concurrently for a set number of ticks
+		TasksComplete := 0
+		for range ticker.C {
+			if TasksComplete == count {
+				break
+			}
+			go func() {
+				TasksComplete++
+				task()
+			}()
+		}
 	}
-	return Parser[filepath.Ext(path)].Translate("path")
 
-}
+	func main(){
+		Get all the bits together so you can close over 'em.
+		resultChannel := make(chan Response)
+		count := 100
 
 
+		DoConcurrentTask(func() {
+			resultChannel <- MakeRequest("https://www.google.com")
+		}, count, *ticker)
 
-func main() {
-	Parse("hello.xml")
-	Parse("hello.json")
+
+		// You've done the speedy stuff, now pull stuff out.
+		var responses []Response
+
+		for i := 0; i < count; i++ {
+			result := <-resultChannel
+			responses = append(responses, result)
+		}
+
+		for _, v := range responses{
+			fmt.Println(v.ResponseTime)
+		}
+
+
+		ticker := time.NewTicker(100 * time.Millisecond)
+		var results []UserJourneyResult
+		resultChannel := make(chan UserJourneyResult)
+		count := 10
+
+		var urls = []string{"https://www.google.com", "https://wwww.tatler.com"}
+
+		DoConcurrentTask(func(){
+					resultChannel <- WalkJourney(urls)},
+					count, *ticker)
+
+
+		for i := 0; i < count; i++ {
+			result := <-resultChannel
+			results = append(results, result)
+		}
+
+		fmt.Println("Done unpacking")
+
+		// Now let's go through all of the codes you received in the process.
+		for _, v := range results{
+			fmt.Println(v.Codes)
+		}
 }
