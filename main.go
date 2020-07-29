20ab01882cf38304c0bb5a975cfc9826a3ba15cc
David Godwin <dgodwin26@yahoo.co.uk>
2020-07-29 16:40:01 +0100

    Clean idea of how we might do a load test

--- main.go
+++ main.go
@@ -1,23 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"time"
 )
 
-	func DoConcurrentTask(task func(), count int, ticker time.Ticker) {
-		// DoConcurrentTask takes in a function and run it concurrently for a set number of ticks
-		TasksComplete := 0
-		for range ticker.C {
-			if TasksComplete == count {
-				break
-			}
-			go func() {
-				TasksComplete++
-				task()
-			}()
-		}
-	}
 
 	func main(){
 		//Get all the bits together so you can close over 'em.
@@ -37,8 +23,5 @@ import (
 			responses = append(responses, result)
 		}
 
-		for _, v := range responses{
-			fmt.Println(v.Codes)
-		}
 
 }
