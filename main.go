47ebd7ac42e0df3e5ae16fa5e75f4cdaec436291
David Godwin <dgodwin26@yahoo.co.uk>
2020-07-31 16:40:39 +0100

    Adding a bit of structure to main for later production run

--- main.go
+++ main.go
@@ -4,29 +4,28 @@ import (
 	"time"
 )
 
+func main() {
+	//Parse the file.
 
-	func main(){
-		//Parse the file.
+	//Validate that everything in the file is okay.
 
+	//Now get prepare the test.
+	ticker := time.NewTicker(100 * time.Millisecond) //TODO: take from config.
+	count := 100                                     //TODO: take from the config.
+	resultChannel := make(chan UserJourneyResult)
+	//var urls []string //TODO: take from config.
 
-		//Now get prepare the test.
-		ticker := time.NewTicker(100*time.Millisecond) //TODO: take from config.
-		count := 100 //TODO: take from the config.
-		resultChannel := make(chan UserJourneyResult)
-		//var urls []string //TODO: take from config.
+	DoConcurrentTask(func() {
+		resultChannel <- WalkJourney([]string{"there should be some urls here"})
+	}, count, *ticker)
 
-		DoConcurrentTask(func() {
-			resultChannel <- WalkJourney([]string{"there should be some urls here"})
-		}, count, *ticker)
+	// You've done the speedy stuff, now unload from the channel.
+	var responses []UserJourneyResult
+	for i := 0; i < count; i++ {
+		result := <-resultChannel
+		responses = append(responses, result)
+	}
 
-		// You've done the speedy stuff, now unload from the channel.
-		var responses []UserJourneyResult
-		for i := 0; i < count; i++ {
-			result := <-resultChannel
-			responses = append(responses, result)
-		}
-
-		//Now prepare the data.
-
+	//Now prepare the data.
 
 }
