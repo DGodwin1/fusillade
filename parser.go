c2c785e69e23de8c490988920089d4523e7811d1
David Godwin <dgodwin26@yahoo.co.uk>
2020-08-07 14:03:36 +0100

    Get JSON parser working. Remove XML parser implementation for now. We should be thinking about this iteratively. Get something of value working end-to-end in JSON and then look to update with XML later. Quite a number of the changes revolve around what we pass into what / what we return. Also, remember, capitals are required in the Go config struct otherwise json.unmarshal won't pay attention to them. Why? Because capitals denote exported fields and unmarshal will only look at exported fields.

--- parser.go
+++ parser.go
@@ -1,72 +1,77 @@
 package main
+
 // This file deals with all things config and parser.
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
-type Config struct{
+type Config struct {
 	// Represents a parsed configuration file.
 	// Regardless of how a parser is implemented
-	urls []string
-	count int
+	Urls  []string `json:"urls"`
+	Count int `json:"hits"`
 }
 
-type Parser interface{
+type Parser interface {
 	// All Parsers should have one method method, Translate() that
 	// takes a filepath and creates a Config. There can and ought to be
 	// multiple Parser implementations that can work with many
 	// different file types.
-	Translate(filepath string) Config
+	Translate(bytesData []byte) *Config
 }
 
 type JSONParser struct{}
 
-func (j JSONParser) Translate(file string) Config{
-	//
-	//parsed := &Config{}
-	//
-	//err := json.Unmarshal(bytesData, parsed)
-	//
-	//if err != nil {
-	//	fmt.Println("error", err)
-	//}
+func (JSONParser) Translate(bytesData []byte) *Config {
 
+	parsed := &Config{}
 
-	return Config{[]string{"JSON PARSED FILE"}, 2}
-}
+	err := json.Unmarshal(bytesData, parsed)
 
-type XMLParser struct{}
+	if err != nil {
+		fmt.Println("error", err)
+	}
 
-func (x XMLParser) Translate(file string) Config{
-	fmt.Println("XML parsing baby")
-	return Config{[]string{"XML parsing"}, 5}
+	return parsed
 }
 
 
-
-func GetParser(extension string) (Parser, error){
+func GetParser(path string) (Parser, error) {
 	Parser := map[string]Parser{
-		".xml":XMLParser{},
-		".json":JSONParser{},
+		".json": JSONParser{},
 		//Add additional parsers here. They need to implement Parser interface.
 	}
 
 	// Before giving a parser back, let's make sure we actually have one to offer
-	p, ok := Parser[extension]
-	if !ok{
+	p, ok := Parser[filepath.Ext(path)]
+	if !ok {
 		return nil, errors.New("there isn't a parser available for this type of file")
 	}
 	return p, nil
 }
 
-func ParseConfigFile(path string) Config{
-	parser, err := GetParser(path)
+func ParseConfigFile(path string) (*Config, error) {
+	// Get the file
+	file, err := os.Open(path)
+
 	if err != nil{
-		//TODO handle the error.
+		fmt.Println(err)
+
 	}
+	defer file.Close()
 
-	return parser.Translate(path)
+	parser, _ := GetParser(path)
+
+	FileInBytes, _ := ioutil.ReadAll(file)
+
+	parsed := &Config{}
+	// Do the parsing now
+	parsed = parser.Translate(FileInBytes)
+
+	return parsed, nil
 }
-
-
