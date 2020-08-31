2f7f3146524800ab882102ea6286c746ba8d7920
David Godwin <dgodwin26@yahoo.co.uk>
2020-08-31 16:57:33 +0100

    Some main{} experiments. Refactoring WalkJourney argument. Add sort function for timestamps. Messing about with some JSON so that I know how to save to a JSON file. WalkJourney now takes a SessionPauser. Nice thing here is that implementation will call PauseSession() and the type's implementation of that will handle how to pause a session. No more passing in parameters for amount to pause :yay: Sort function in main{} is to ensure that the latencies vals passed to the bar grapher maker are being passed in sorted order. Otherwise, the bar graph will order the bars, left to right, based on which user journey finished first and that's wrong. It should receive a packet of data where the first item represents the first user journey launched.

--- main.go
+++ main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"sort"
 	"time"
 )
 
 func main() {
 	//TODO: get the config from _somewhere
 	// Get the config.
-
-
-	// Parse the file.
+	//
+	//Parse the file.
 	config, err := ParseConfigFile("test_config.json")
 
 	if err != nil{
-		fmt.Println("There's been an issue parsing the config file.")
-		fmt.Printf("Here's the error %q", err)
+		fmt.Printf("error parsing config: %q", err)
 		return
 	}
 
-	//Validate that everything in the file is okay.
+	// Validate that everything in the file is okay.
 	v := ConfigValidator{}
 	_, err = v.Validate(config)
 
 	if err != nil{
-		fmt.Println("There's been an issue validating the configuration.")
-		fmt.Printf("Here's the error %q", err)
+		fmt.Printf("Error with validating config: %q", err)
 		return
 	}
 
-	//Prepare the test.
+	// Prepare the test.
 	urls := config.Urls
 	count := config.Count
 	ticker := time.NewTicker(100 * time.Millisecond) //TODO: take from config.
 
 	resultChannel := make(chan UserJourneyResult)
 
-	reader := EndUserReader{}
-	MillisecondReadingTime := 10000
+	user := FakeUser{DelayTime: 100}
 
 	// Hit the URLS
 	DoConcurrentTask(func() {
-		resultChannel <- WalkJourney(urls, reader, MillisecondReadingTime)
+		resultChannel <- WalkJourney(urls, user)
 	}, count, *ticker)
 
 
@@ -52,10 +52,24 @@ func main() {
 		responses = append(responses, result)
 	}
 
-	for _, v := range responses {
-		fmt.Println(v.JourneyResponseTimeMS)
-	}
-	//Now prepare a report
-	MakeBarGraph(responses)
+	// Sort the responses by when the UserJourney actually started.
+	sort.Slice(responses, func(i, j int) bool{
+		return responses[i].JourneyStart.Before(responses[j].JourneyStart)
+	})
 
+	//Now prepare a report
+	//MakeBarGraph(responses)
+	//MakePieChart(responses)
+
+	var jsonData []byte
+	jsonData, err = json.MarshalIndent(responses, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(string(jsonData))
+
+	err = ioutil.WriteFile("output.json", jsonData, 0644)
+
+	fmt.Println("thank god for that")
 }
+
