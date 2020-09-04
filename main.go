28076658828823d2598e80f133d3f00cf3e1130a
David Godwin <dgodwin26@yahoo.co.uk>
2020-09-04 21:32:58 +0100

    start using flags for non-test-based configuration

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"sort"
-	"time"
 )
 
 func main() {
+	//Line up the flags.
+	configFile := flag.String("config", "", "provide config location")
+	wantsGraphs := flag.Bool("graphOutput", false, "does user want graphs and stuff")
+	wantsJSONOutput := flag.Bool("outJSON?", false, "does the user want a JSON representation of the report")
+	flag.Parse()
+	fmt.Println("configFile", *configFile)
+	fmt.Println("wants chart:", *wantsGraphs)
+	fmt.Println("wants JSON:", *wantsJSONOutput)
 
 	//Parse the file.
-	config, err := ParseConfigFile("test_config.json")
+	config, err := ParseConfigFile(*configFile)
 
 	if err != nil {
 		fmt.Printf("error parsing config: %q", err)
@@ -25,58 +32,61 @@ func main() {
 		return
 	}
 
-	// Prepare the test.
-	urls := config.Urls
-	count := config.Count
-	rate := time.Duration(config.Rate)
-	ticker := time.NewTicker(rate * time.Millisecond)
-
-	resultChannel := make(chan UserJourneyResult)
-
-	user := FakeUser{DelayTime: config.PauseLength}
-
-	// Hit the URLS
-	DoConcurrentTask(func() {
-		resultChannel <- WalkJourney(urls, user)
-	}, count, *ticker)
-
-	// You've done the speedy stuff, now unload from the channel.
-	var responses []UserJourneyResult
-	for i := 0; i < count; i++ {
-		result := <-resultChannel
-		responses = append(responses, result)
-	}
-
-	// Sort the responses by when the UserJourney actually started.
-	sort.Slice(responses, func(i, j int) bool {
-		return responses[i].JourneyStart.Before(responses[j].JourneyStart)
-	})
-
-	//Now prepare a latency report
-	var latencies []int
-	var xAxis []int
-	// Get hold of the latency values so that you can put them into the graph.
-	for _, v := range responses {
-		latencies = append(latencies, int(v.JourneyResponseTimeMS))
-	}
-
-	// Number each user journey value
-	for i := 1; i <= len(latencies); i++ {
-		xAxis = append(xAxis, i)
-	}
-
-	MakeBarGraph("Latencies", "latencies.html", "Latency", xAxis, latencies)
-
-	//Prepare a count for the ResponseCodeCount
-	var codeXAxis []int
-	var countYAxis []int
-
-	for code, count := range CountResponseCodes(responses) {
-		codeXAxis = append(codeXAxis, code)
-		countYAxis = append(countYAxis, count)
-	}
-
-	MakeBarGraph("ResponseCodeCount", "responseCount.html", "Count", codeXAxis, countYAxis)
+	fmt.Println(config.Urls, config.Rate, config.PauseLength, config.Count)
+}
+	//
+	//// Prepare the test.
+	//urls := config.Urls
+	//count := config.Count
+	//rate := time.Duration(config.Rate)
+	//ticker := time.NewTicker(rate * time.Millisecond)
+	//
+	//resultChannel := make(chan UserJourneyResult)
+	//
+	//user := FakeUser{DelayTime: config.PauseLength}
+	//
+	//// Hit the URLS
+	//DoConcurrentTask(func() {
+	//	resultChannel <- WalkJourney(urls, user)
+	//}, count, *ticker)
+	//
+	//// You've done the speedy stuff, now unload from the channel.
+	//var responses []UserJourneyResult
+	//for i := 0; i < count; i++ {
+	//	result := <-resultChannel
+	//	responses = append(responses, result)
+	//}
+	//
+	//// Sort the responses by when the UserJourney actually started.
+	//sort.Slice(responses, func(i, j int) bool {
+	//	return responses[i].JourneyStart.Before(responses[j].JourneyStart)
+	//})
+	//
+	////Now prepare a latency report
+	//var latencies []int
+	//var xAxis []int
+	//// Get hold of the latency values so that you can put them into the graph.
+	//for _, v := range responses {
+	//	latencies = append(latencies, int(v.JourneyResponseTimeMS))
+	//}
+	//
+	//// Number each user journey value
+	//for i := 1; i <= len(latencies); i++ {
+	//	xAxis = append(xAxis, i)
+	//}
+	//
+	//MakeBarGraph("Latencies", "latencies.html", "Latency", xAxis, latencies)
+	//
+	////Prepare a count for the ResponseCodeCount
+	//var codeXAxis []int
+	//var countYAxis []int
+	//
+	//for code, count := range CountResponseCodes(responses) {
+	//	codeXAxis = append(codeXAxis, code)
+	//	countYAxis = append(countYAxis, count)
+	//}
+	//
+	//MakeBarGraph("ResponseCodeCount", "responseCount.html", "Count", codeXAxis, countYAxis)
 
 	//var jsonData []byte
 	//jsonData, err = json.MarshalIndent(responses, "", "     ")
@@ -87,4 +97,4 @@ func main() {
 	//
 	//err = ioutil.WriteFile("output.json", jsonData, 0644)
 	//
-}
+//}
