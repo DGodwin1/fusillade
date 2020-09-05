e890ef2e033dd95dff47cda04953c81723c9c05f
David Godwin <dgodwin26@yahoo.co.uk>
2020-09-05 12:30:30 +0100

    generate fakeJSON if user doesn’t provide config. ‘listen’ to flags. If user wants graphs, we make them. if user wants JSON output, we make it. also output summative data.

--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"flag"
 	"fmt"
+	"sort"
+	"time"
 )
 
 func main() {
 	//Line up the flags.
-	configFile := flag.String("config", "", "provide config location")
-	wantsGraphs := flag.Bool("graphOutput", false, "does user want graphs and stuff")
-	wantsJSONOutput := flag.Bool("outJSON?", false, "does the user want a JSON representation of the report")
+	configFile := flag.String("c", "", "user to provide config")
+	wantsGraphs := flag.Bool("g", false, "does user want data visulisation?")
+	wantsJSON := flag.Bool("j", false, "does user want to save JSON of responses")
 	flag.Parse()
-	fmt.Println("configFile", *configFile)
-	fmt.Println("wants chart:", *wantsGraphs)
-	fmt.Println("wants JSON:", *wantsJSONOutput)
+
+	if *configFile == ""{
+		//generate fake JSON
+		fmt.Println(`This tool needs a configuration file to work from.
+To make things easier, I have made one called 'config.json'
+which you can edit and use. Once you're happy, simply re-run the
+program with the -config flag set to 'config.json'.`)
+
+		//create the file as promised:
+		MakeFakeJSONFile()
+		return
+	}
 
 	//Parse the file.
 	config, err := ParseConfigFile(*configFile)
@@ -28,73 +39,76 @@ func main() {
 	_, err = v.Validate(config)
 
 	if err != nil {
-		fmt.Printf("Error with validating config: %q", err)
+		fmt.Printf("error validating config: %q", err)
 		return
 	}
 
-	fmt.Println(config.Urls, config.Rate, config.PauseLength, config.Count)
-}
-	//
-	//// Prepare the test.
-	//urls := config.Urls
-	//count := config.Count
-	//rate := time.Duration(config.Rate)
-	//ticker := time.NewTicker(rate * time.Millisecond)
-	//
-	//resultChannel := make(chan UserJourneyResult)
-	//
-	//user := FakeUser{DelayTime: config.PauseLength}
-	//
-	//// Hit the URLS
-	//DoConcurrentTask(func() {
-	//	resultChannel <- WalkJourney(urls, user)
-	//}, count, *ticker)
-	//
-	//// You've done the speedy stuff, now unload from the channel.
-	//var responses []UserJourneyResult
-	//for i := 0; i < count; i++ {
-	//	result := <-resultChannel
-	//	responses = append(responses, result)
-	//}
-	//
-	//// Sort the responses by when the UserJourney actually started.
-	//sort.Slice(responses, func(i, j int) bool {
-	//	return responses[i].JourneyStart.Before(responses[j].JourneyStart)
-	//})
-	//
-	////Now prepare a latency report
-	//var latencies []int
-	//var xAxis []int
-	//// Get hold of the latency values so that you can put them into the graph.
-	//for _, v := range responses {
-	//	latencies = append(latencies, int(v.JourneyResponseTimeMS))
-	//}
-	//
-	//// Number each user journey value
-	//for i := 1; i <= len(latencies); i++ {
-	//	xAxis = append(xAxis, i)
-	//}
-	//
-	//MakeBarGraph("Latencies", "latencies.html", "Latency", xAxis, latencies)
-	//
-	////Prepare a count for the ResponseCodeCount
-	//var codeXAxis []int
-	//var countYAxis []int
-	//
-	//for code, count := range CountResponseCodes(responses) {
-	//	codeXAxis = append(codeXAxis, code)
-	//	countYAxis = append(countYAxis, count)
-	//}
-	//
-	//MakeBarGraph("ResponseCodeCount", "responseCount.html", "Count", codeXAxis, countYAxis)
+	// Prepare the test based on the values stored in the config.
+	urls := config.Urls
+	count := config.Count
+	rate := time.Duration(config.Rate)
+	ticker := time.NewTicker(rate * time.Millisecond)
 
-	//var jsonData []byte
-	//jsonData, err = json.MarshalIndent(responses, "", "     ")
-	//if err != nil {
-	//	log.Println(err)
-	//}
-	//fmt.Println(string(jsonData))
-	//
-	//err = ioutil.WriteFile("output.json", jsonData, 0644)
-	//
-//}
+	resultChannel := make(chan UserJourneyResult)
+
+	user := FakeUser{DelayTime: config.PauseLength}
+
+	// Hit the URLS
+	DoConcurrentTask(func() {
+		resultChannel <- WalkJourney(urls, user)
+	}, count, *ticker)
+
+	// You've done the speedy stuff, now unload from the channel.
+	var responses []UserJourneyResult
+	for i := 0; i < count; i++ {
+		result := <-resultChannel
+		responses = append(responses, result)
+	}
+
+	// Now sort the responses based on when the user journey was launched
+	// otherwise it's based on when journeys completed.
+	sort.Slice(responses, func(i, j int) bool {
+		return responses[i].JourneyStart.Before(responses[j].JourneyStart)
+	})
+
+	if *wantsGraphs{
+		// Make with the data vis.
+		//Now prepare a latency report
+		var latencies []int
+		var xAxis []int
+
+		// Get hold of the latency values so that you can put them into the graph.
+		for _, v := range responses {
+			latencies = append(latencies, int(v.JourneyResponseTimeMS))
+		}
+
+		// Number each user journey value
+		for i := 1; i <= len(latencies); i++ {
+			xAxis = append(xAxis, i)
+		}
+
+		MakeBarGraph("Latencies", "latencies.html", "Latency in MS", xAxis, latencies)
+
+		//Prepare a count for the ResponseCodeCount
+		var codeXAxis []int
+		var countYAxis []int
+
+		for code, count := range CountResponseCodes(responses) {
+			codeXAxis = append(codeXAxis, code)
+			countYAxis = append(countYAxis, count)
+		}
+
+		MakeBarGraph("ResponseCodeCount", "responseCount.html", "Response code count", codeXAxis, countYAxis)
+	}
+
+	if *wantsJSON{
+		ConstructJSONReport(responses)
+	}
+
+	fmt.Println("Load test complete.")
+	d := ConstructSummativeStats(responses)
+	fmt.Println("Max response time:", d.MinJourneyResponse)
+	fmt.Println("Min response time:", d.MaxJourneyResponse)
+	fmt.Println("Min response time:", d.ResponseCodeCount)
+
+}
